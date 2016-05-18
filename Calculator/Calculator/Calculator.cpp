3dcc9a59448eca7db78e1168084239165baace47
Gordon Vaynapel <gordonvaynapel@gmail.com>
2016-05-18 08:12:38 -0700

    messing around with headers/multiple files

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -21,7 +21,7 @@ int main()
 			cout << "Enter Value two" << endl;
 			cin >> b;
 			cout << "Your Answer is:" << a + b << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 		if (x == 2) {
 			cout << "Enter Value one" << endl;
@@ -29,7 +29,7 @@ int main()
 			cout << "Enter Value two" << endl;
 			cin >> b;
 			cout << "Your Answer is:" << a - b << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 		if (x == 3) {
 			cout << "Enter Value one" << endl;
@@ -37,7 +37,7 @@ int main()
 			cout << "Enter Value two" << endl;
 			cin >> b;
 			cout << "Your Answer is:" << a * b << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 		if (x == 4) {
 			cout << "Enter Value one" << endl;
@@ -45,13 +45,13 @@ int main()
 			cout << "Enter Value two" << endl;
 			cin >> b;
 			cout << "Your Answer is:" << a / b << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 		if (x == 5) {
 			cout << "Enter Value to be square rooted" << endl;
 			cin >> a;
 			cout << "Your Answer is:" << std::sqrt(a) << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 		if (x == 6) {
 			cout << "Enter Value" << endl;
@@ -59,7 +59,7 @@ int main()
 			cout << "Enter the value of what power you wish to do" << endl;
 			cin >> b; 
 			cout << "Your Answer is:" << std::pow(a,b) << endl;
-			Sleep(1000);
+			Sleep(3000);
 		}
 	}
 	return 0;
